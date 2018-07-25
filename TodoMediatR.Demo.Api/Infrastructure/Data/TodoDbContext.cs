6efd40ed8d4df09a9aa5a658d8627bc7d24db56f
Mario Vargas <mvargas@cardinalsolutions.com>
2018-07-24 20:52:07 -0400

    Add Entity Framework Core

---
+++ TodoMediatR.Demo.Api/Infrastructure/Data/TodoDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApiMediatR.Demo.Api.Infrastructure.Data 
+{
+    public class TodoDbContext : DbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItem { get; set; }
+    }
+}
--- TodoMediatR.Demo.Api/Startup.cs
+++ TodoMediatR.Demo.Api/Startup.cs
@@ -10,7 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 using MediatR;
+using TodoApiMediatR.Demo.Api.Infrastructure.Data;
 
 namespace TodoApiMediatR.Demo.Api
 {
@@ -26,6 +28,8 @@ namespace TodoApiMediatR.Demo.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoDbContext>(options => options.UseInMemoryDatabase("TodoList"));
+
             services.AddMediatR(typeof(Startup).Assembly);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
